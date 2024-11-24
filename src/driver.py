9abf5d2e54dda0c692c1f9f198e359b180440cfe
Kirgnition <riccardobuah@gmail.com>
2024-11-24 22:47:41 +0100

    improve direction handling

--- src/driver.py
+++ src/driver.py
@@ -1,15 +1,17 @@
 from sphero_sdk import SerialAsyncDal
 from sphero_sdk import SpheroRvrAsync
 from src.utils.directions import Direction
+import asyncio
 
 class RvrDriver:
     def __init__(self, loop):
         self.loop = loop
-        self.rvr = SpheroRvrAsync(dal = SerialAsyncDal(loop))
+        self.rvr = SpheroRvrAsync(dal = SerialAsyncDal(self.loop))
+        self.heading_step = 10
+
         self.speed = 0
         self.heading = 0
         self.flags = 0
-        self.heading_step = 10
     
     async def wake(self):
         """Wake up the RVR and reset its yaw."""
@@ -18,6 +20,8 @@ class RvrDriver:
     
     def update_controls(self, proto_message):
         """Drive the RVR at the given speed and direction."""
+        if (len(proto_message.directions) == 0):
+            return
         direction = proto_message.directions[0]
         self.speed = proto_message.speed
         if direction == Direction.FORWARD:
@@ -25,9 +29,9 @@ class RvrDriver:
         elif direction == Direction.BACKWARD:
             self.flags = 1
         elif direction == Direction.LEFT:
-            self.heading -= self.heading_step
-        elif direction == Direction.RIGHT:
             self.heading += self.heading_step
+        elif direction == Direction.RIGHT:
+            self.heading -= self.heading_step
         else:
             self.speed = 0
             self.flags = 0
@@ -39,5 +43,5 @@ class RvrDriver:
 
     async def drive(self):
         """Drive the RVR according to the current controls."""
-        await self.rvr.drive_with_heading(speed = self.speed, heading = self.heading, flags = self.flags)      
-    
+        await self.rvr.drive_with_heading(speed = self.speed, heading = self.heading, flags = self.flags)
+        await asyncio.sleep(0.1)
